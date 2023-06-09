95ed038bc29d1a25b6439cf4c3ce4b557a73e00b
Will Hansen <will.r.s.hansen@gmail.com>
2023-06-09 16:12:47 -0700

    improve image diff

--- word-diff.zsh
+++ word-diff.zsh
@@ -9,21 +9,21 @@ convert -size 500x200 xc:white -font DejaVu-Sans -pointsize 150 \
 convert -size 500x200 xc:white -font DejaVu-Sans -pointsize 150 \
         -gravity northwest -fill black -annotate +0+0 $B B.png && \
 
-convert A.png B.png -compose Minus_Src -composite -negate B_only.png && \
-convert B.png A.png -compose Minus_Src -composite -negate A_only.png && \
-convert A.png B.png -compose Plus -composite C_only.png && \
+convert B.png -compose Minus A.png -composite -negate B_only.png && \
+convert A.png -compose Minus B.png -composite -negate A_only.png && \
+convert A.png -compose Lighten B.png -composite C_only.png && \
 
-rm A.png B.png && \
+#rm A.png B.png && \
 
 convert A_only.png +level-colors DodgerBlue3, A_only_blue.png && \
 convert B_only.png +level-colors firebrick, B_only_red.png && \
 
-rm A_only.png B_only.png && \
+#rm A_only.png B_only.png && \
 
 convert A_only_blue.png B_only_red.png -compose Darken -composite diff.png && \
 convert C_only.png diff.png -compose Darken -composite ${A}"_vs_"${B}".png" && \
 
-rm A_only_blue.png B_only_red.png C_only.png diff.png
+echo a #rm A_only_blue.png B_only_red.png C_only.png diff.png
 
 #convert A.png B.png -compose Darken -composite first_and_second_only.png && \
 #composite -compose difference A.png B.png difference.png && \
