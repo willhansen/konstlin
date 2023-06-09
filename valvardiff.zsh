9c9fbfc4001b285e37738776b204c57cfe450da2
Will Hansen <will.r.s.hansen@gmail.com>
2023-06-08 23:08:57 -0700

    Add kotlin as subproject, and improve the image script

--- valvardiff.zsh
+++ valvardiff.zsh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-A="konst"
-B="var"
+A="$1"
+B="$2"
+
 
 convert -size 500x200 xc:white -font DejaVu-Sans -pointsize 150 \
         -gravity northwest -fill black -annotate +0+0 $A A.png && \
@@ -10,12 +11,20 @@ convert -size 500x200 xc:white -font DejaVu-Sans -pointsize 150 \
 
 convert A.png B.png -compose Minus_Src -composite -negate B_only.png && \
 convert B.png A.png -compose Minus_Src -composite -negate A_only.png && \
-convert A.png B.png -compose Plus -composite C_only.png 
+convert A.png B.png -compose Plus -composite C_only.png && \
 
-convert A_only.png +level-colors blue, A_only_blue.png
-convert B_only.png +level-colors red, B_only_red.png
-convert A_only_blue.png B_only_red.png -compose Darken -composite diff.png 
-convert C_only.png diff.png -compose Darken -composite final.png 
+rm A.png B.png && \
+
+convert A_only.png +level-colors blue, A_only_blue.png && \
+convert B_only.png +level-colors red, B_only_red.png && \
+
+rm A_only.png B_only.png && \
+
+convert A_only_blue.png B_only_red.png -compose Darken -composite diff.png && \
+convert C_only.png diff.png -compose Darken -composite ${A}"_vs_"${B}".png" && \
+
+rm A_only_blue.png B_only_red.png C_only.png diff.png
+
 #convert A.png B.png -compose Darken -composite first_and_second_only.png && \
 #composite -compose difference A.png B.png difference.png && \
 #composite difference.png A.png output.png && \
